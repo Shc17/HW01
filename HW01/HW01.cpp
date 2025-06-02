64217b06dd22e81021801867e5d5013c031d64dc
Hyeongcheol <gudcjf6543@gmail.com>
2025-06-02 15:33:06 +0900

    First

---
+++ HW01/HW01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int gettot(int num[], int  in)
+{
+	int sum = 0;
+	for (int i = 0; i < in; i++)
+	{
+		sum += num[i];
+	}
+	return sum;
+}
+
+int getavg(int num[], int in)
+{
+	int sum = gettot(num, in);
+	return sum / in;
+}
+
+int main()
+{
+	int num[5];
+	int in = 5;
+
+	for (int i = 0; i < in; i++)
+	{
+		cout << "number:";
+		cin >> num[i];
+	}
+
+	int tot = gettot(num, in);
+	int avg = getavg(num, in);
+
+	cout << "tot:" << tot << " agv:" << avg << endl;
+
+	return 0;
+
+}
